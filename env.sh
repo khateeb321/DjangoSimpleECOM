338a718a1c2e241d4a20179f4fca60d06d1d8de6
Ahmed Khateeb <khateeb321@gmail.com>
2018-09-30 14:43:59 +0500

    Set up base files

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export SECRET_KEY='w#a2st*zypodd+4cxdn6h*16tdhctll-ws#9n3loejt59w1k5i'
+export DEBUG='True'
+
+export STRIPE_PUBLISHABLE_KEY='pk_test_D8LmMZeMzOVfijTx0iZijYXR'
+export STRIPE_SECRET_KEY='sk_test_nosB42kLHy53yocK3TRe5mRU'
