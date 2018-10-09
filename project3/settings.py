8c1b05b99b016e13b7a2beae6e1f808fcf58697a
Ahmed Khateeb <khateeb321@gmail.com>
2018-10-09 12:40:38 +0500

    test

--- project3/settings.py
+++ project3/settings.py
@@ -93,12 +93,7 @@ WSGI_APPLICATION = 'project3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
