5fbce8a0581af9f38b2a0438039319c35e42279b
Ahmed Khateeb <khateeb321@gmail.com>
2018-10-10 14:28:19 +0500

    clean

--- env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export SECRET_KEY='g$^o@6fi@j71_$q2qjjqj(*873f%bd4bg4%k*6foyufx1df#t&'
-export DEBUG='True'
-
-export STRIPE_PUBLISHABLE_KEY='pk_test_D8LmMZeMzOVfijTx0iZijYXR'
-export STRIPE_SECRET_KEY='sk_test_nosB42kLHy53yocK3TRe5mRU'
