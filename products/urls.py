351b1742847af02e4ed137e1c40820b74c682b88
Ahmed Khateeb <khateeb321@gmail.com>
2018-10-19 16:58:02 +0500

    prodcuts crud base

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url, include
 from .views import all_products
+from products import views
 
 urlpatterns = [
-    url(r'^$', all_products, name='products')
+    url(r'^$', all_products, name='products'),
+    url(r'^emp$', views.emp),
+    url(r'^show$', views.show),
+    url(r'^edit/<int:id>$', views.edit),
+    url(r'^update/<int:id>$', views.update),
+    url(r'^delete/<int:id>$', views.destroy),
     ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from products.forms import ProductForm
 
 # Create your views here.
+
+
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+
+
+def emp(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/show')
+            except:
+                pass
+    else:
+        form = ProductForm()
+    return render(request, 'index.html', {'form': form})
+
+
+def show(request):
+    products = Product.objects.all()
+    return render(request, "show.html", {'products': products})
+
+
+def edit(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'edit.html', {'product': product})
+
+
+def update(request, id):
+    product = Product.objects.get(id=id)
+    form = Product(request.POST, instance=product)
+    if form.is_valid():
+        form.save()
+        return redirect("/show")
+    return render(request, 'edit.html', {'product': product})
+
+
+def destroy(request, id):
+    product = Product.objects.get(id=id)
+    product.delete()
+    return redirect("/show")
